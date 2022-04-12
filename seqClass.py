38e09c030877ce0e469f3b8804d6d7b5c542fccd
carlafeliu <100620721+carlafeliu@users.noreply.github.com>
2022-04-12 10:55:17 +0100

    Add files via upload

---
+++ seqClass.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys, re
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
+parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+args = parser.parse_args()
+
+if re.search('^[ACGTU]+$', args.seq):
+    if re.search('T', args.seq):
+        print ('The sequence is DNA')
+    elif re.search('U', args.seq):
+        print ('The sequence is RNA')
+    else:
+        print ('The sequence can be DNA or RNA')
+else:
+    print ('The sequence is not DNA nor RNA')
+
+if args.motif:
+    args.motif = args.motif.upper()
+    print ('Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}" ... ', end = '')
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
+
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+
+    
